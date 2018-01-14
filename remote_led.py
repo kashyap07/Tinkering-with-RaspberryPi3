01b7efb48740cbf14ccf4ece10e30a308e512c45
Suhas Kashyap <kashyapsuhas07@gmail.com>
2018-01-14 21:59:13 +0530

    Basic LED

---
+++ remote_led.py
@@ -0,0 +1,17 @@
+'''
+renote applicances
+'''
+
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+
+print('LED ON')
+GPIO.output(18, GPIO.HIGH)
+time.sleep(1)
+
+print('LED OFF')
+GPIO.output(18, GPIO.LOW)
